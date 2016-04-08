c2cf3967e45fdce9c5d14d3319bdfac8813cf4f8
Tyler Tidman <tyler.tidman@draak.ca>
2016-04-08 17:04:37 -0400

    Autopep8 old implementation

--- ll2mh.py
+++ ll2mh.py
@@ -6,50 +6,56 @@
 
 # rkanters 2004.2.20 version ll2mh
 
-import re,sys,string
-if len(sys.argv)==2: # slob city
-    stringlength=string.atoi(sys.argv[1])
-    if stringlength<2 or stringlength%2!=0:
+import re
+import sys
+import string
+if len(sys.argv) == 2:  # slob city
+    stringlength = string.atoi(sys.argv[1])
+    if stringlength < 2 or stringlength % 2 != 0:
         sys.stderr.write('string length requested must be even integer > 0\n')
         sys.exit(87)
 else:
-    stringlength=6
-maxn=stringlength/2
-A=ord('A')
+    stringlength = 6
+maxn = stringlength / 2
+A = ord('A')
 while 1:
-    line=sys.stdin.readline()
-    if not line: break
-    ll=re.findall(r'([-0-9.]+)\s+([-0-9.]+)',line)
+    line = sys.stdin.readline()
+    if not line:
+        break
+    ll = re.findall(r'([-0-9.]+)\s+([-0-9.]+)', line)
     if ll:
-        for x,y in ll:
-            lon=string.atof(x)
-            lat=string.atof(y)
+        for x, y in ll:
+            lon = string.atof(x)
+            lat = string.atof(y)
     else:
-        sys.stderr.write(sys.argv[0]+': cannot even get the basic items\n')
+        sys.stderr.write(sys.argv[0] + ': cannot even get the basic items\n')
         sys.exit(44)
-    if -180<=lon<180:pass
+    if -180 <= lon < 180:
+        pass
     else:
         sys.stderr.write('longitude must be -180<=lon<180\n')
         sys.exit(32)
-    if -90<=lat<90:pass
+    if -90 <= lat < 90:
+        pass
     else:
         sys.stderr.write('latitude must be -90<=lat<90\n')
-        sys.exit(33) #can't handle north pole, sorry, [A-R]
-    lon=(lon+180.0)/20 # scale down and set up for first digit
-    lat=(lat+90.0)/10
-    astring=""
-    i=0
-    while i<maxn:
-        i+=1
-        loni=int(lon)
-        lati=int(lat)
-        if i%2:
-            astring+=chr(A+loni)+chr(A+lati)
-            lon=(lon-loni)*10
-            lat=(lat-lati)*10
+        sys.exit(33)  # can't handle north pole, sorry, [A-R]
+    lon = (lon + 180.0) / 20  # scale down and set up for first digit
+    lat = (lat + 90.0) / 10
+    astring = ""
+    i = 0
+    while i < maxn:
+        i += 1
+        loni = int(lon)
+        lati = int(lat)
+        if i % 2:
+            astring += chr(A + loni) + chr(A + lati)
+            lon = (lon - loni) * 10
+            lat = (lat - lati) * 10
         else:
-            astring+=str(loni)+str(lati)
-            lon=(lon-loni)*24
-            lat=(lat-lati)*24
+            astring += str(loni) + str(lati)
+            lon = (lon - loni) * 24
+            lat = (lat - lati) * 24
     print astring
-#We return the grid square, to the precision given, that contains the given point.
+# We return the grid square, to the precision given, that contains the
+# given point.
